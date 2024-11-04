23fc30eaa8a86476ae434baaa777d4b8f034e36e
FritschyMatheo <matheo.fritschy13@gmail.com>
2024-11-04 11:40:08 +0100

    Exo1 fini

--- R3.09/exceptions1.py
+++ R3.09/exceptions1.py
@@ -2,14 +2,15 @@ def divEntier(x: int, y: int) -> int:
     """
     Ce code effectue une simple division de deux nombre entiers et revoie un nombre entier.
     """
-    if x or y < 0:
-        raise ValueError
+    if x < 0 or y < 0:
+        raise ValueError("Les nombres doivent être positifs")
     elif y == 0:
-        raise ZeroDivisionError
+        raise ZeroDivisionError("Erreur de division par zero")
     elif x < y:
         return 0
     else:
         x = x - y
+    
     return divEntier(x, y) + 1
 
 
@@ -21,5 +22,6 @@ if __name__ == "__main__":
     except ValueError as err:
         # On ajoute une ValueError si la personne entre par exemple des lettres
         print(f"Une des valeurs entrées n'est pas valide {err}")
-    except ZeroDivisionError:
-        print("y est égal à 0, l'opération n'est donc mathématiquement pas possible.")
+    except ZeroDivisionError as err:
+        print(f"y est égal à 0, l'opération n'est donc mathématiquement pas possible : {err}")
+        
