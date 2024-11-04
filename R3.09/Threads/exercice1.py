73ac03b8d698adab5ccaeff35758365f6e310c77
FritschyMatheo <matheo.fritschy13@gmail.com>
2024-11-04 16:14:43 +0100

    Exo1 fini

--- R3.09/Threads/exercice1.py
+++ R3.09/Threads/exercice1.py
@@ -1,18 +1,22 @@
 import threading
 import time
 
-def task(i):
-    print(f"Tâche {i} commence pour {i+1} seconde(s)")
-    time.sleep(1)
-    print(f"Tâche {i} terminée")
+def task(i, x):
+    for y in range(x):
+        print(f"Je suis la thread {i}")
+    time.sleep(0.1)
 
-T = []
-
-for i in range(10):
-    T.append(threading.Thread(target=task, args=[i]))
 
 if __name__ == "__main__":
-    for i in range(len(T)):
-        T[i].start()
-    for i in range(len(T)):
-        T[i].join()
+    start = time.perf_counter()
+    t1 = threading.Thread(target=task, args=(1,5))
+    t1.start()
+
+    t2 = threading.Thread(target=task, args=(2,5))
+    t2.start()
+
+    t1.join()
+    t2.join()
+    
+    end = time.perf_counter()
+    print(f"Les tâches ont été exécutées en {round(end - start, 2)} seconde(s)")
