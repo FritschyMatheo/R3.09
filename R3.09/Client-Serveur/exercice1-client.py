e8ea475b38dcbd52a078c77936bfedeac7823dac
FritschyMatheo <matheo.fritschy13@gmail.com>
2024-11-08 16:46:22 +0100

    Exercice 1 Client-serveur update

--- R3.09/Client-Serveur/exercice1-client.py
+++ R3.09/Client-Serveur/exercice1-client.py
@@ -1,14 +1,20 @@
 import socket
 
 client_socket = socket.socket()
-
 port = 10000
-print("Connexion au serveur")
-client_socket.connect(("127.0.0.1", port))
+ip = "127.0.0.1"
+
+print("Connexion au serveur...")
+client_socket.connect((ip, port))
+testco = client_socket.recv(1024).decode()
+print(testco)
+
 message = "Hello!"
 print(f"Envoie du message {message}")
 client_socket.send(message.encode())
+
 reply = client_socket.recv(1024).decode()
 print(f"Reception réponse {reply}")
+
 print("Fermeture de la connexion")
 client_socket.close()
