b02afc72be31639ba95f5c727cccb848445587b7
Mathéo FRITSCHY <matheo.fritschy@altodis.fr>
2024-11-11 17:19:45 +0100

    Exercice 2 bye

--- R3.09/Client-Serveur/exercice2-client.py
+++ R3.09/Client-Serveur/exercice2-client.py
@@ -1,20 +1,35 @@
 import socket
+import time
 
+def bye():
+    """
+    Fonction qui ferme la connexion du client.
+    """
+    print("Fermeture de la connexion client")
+    fermeture = "Fermeture de la connexion client"
+    client_socket.send(fermeture.encode())
+    client_socket.close()
+
+#Configuration connexion
 client_socket = socket.socket()
 port = 10000
 ip = "127.0.0.1"
 
+#Etablissement de la connexion
 print("Connexion au serveur...")
 client_socket.connect((ip, port))
 testco = client_socket.recv(1024).decode()
 print(testco)
 
+#Phase de messages
 message = "Hello!"
-print(f"Envoie du message {message}")
+print(f"Client : {message}")
 client_socket.send(message.encode())
 
 reply = client_socket.recv(1024).decode()
-print(f"Reception réponse {reply}")
+print(f"Server : {reply}")
 
-print("Fermeture de la connexion")
-client_socket.close()
+message = "bye"
+print(f"Client : {message}")
+client_socket.send(message.encode())
+bye()
--- R3.09/Client-Serveur/exercice2-serveur.py
+++ R3.09/Client-Serveur/exercice2-serveur.py
@@ -1,27 +1,33 @@
 import socket
+import time
 
+#Configuration connexion
 serveur_socket = socket.socket()
 port = 10000
 ip = "127.0.0.1"
 
+#Etablissement de la connexion
 serveur_socket.bind((ip, port))
 serveur_socket.listen(1)
 print("En attente de connexion...")
 
 conn, address = serveur_socket.accept()
-acceptation = "La connexion a bien été acceptée"
-
+acceptation = "Connexion au serveur acceptée, vous pouvez communiquer."
 conn.send(acceptation.encode())
-print("Connexion acceptée, attente du message")
+print("Client connecté")
+
+#Phase de messages
+message = conn.recv(1024).decode()
+print(f"Client : {message}")
+
+reply = "Salut client !"
+conn.send(reply.encode())
+print(f"Server : {reply}")
 
 message = conn.recv(1024).decode()
-print(f"Message recu : {message}")
-
-reply = "Yo, j'ai bien recu le message"
-print(f"Envoie réponse : {reply}")
-conn.send(reply.encode())
-print("Réponse envoyée")
-
-print("Fermeture de la connexion")
+print(f"Client : {message}")
+print("Fermeture de la connexion du client")
 conn.close()
+
+print("Fermeture du serveur")
 serveur_socket.close()
