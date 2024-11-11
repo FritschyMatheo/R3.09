bfd65d0369ff65817ed185df2b33f3179756c878
Mathéo FRITSCHY <matheo.fritschy@altodis.fr>
2024-11-11 17:41:45 +0100

    Exo 2 synchrone ok

---
+++ R3.09/Client-Serveur/exercice2-client2.py
@@ -0,0 +1,34 @@
+import socket
+
+def bye():
+    """
+    Fonction qui ferme la connexion du client.
+    """
+    print("Fermeture de la connexion client")
+    fermeture = "Fermeture de la connexion client"
+    client_socket.send(fermeture.encode())
+    client_socket.close()
+
+#Configuration connexion
+client_socket = socket.socket()
+port = 10000
+ip = "127.0.0.1"
+
+#Etablissement de la connexion
+print("Connexion au serveur...")
+client_socket.connect((ip, port))
+testco = client_socket.recv(1024).decode()
+print(testco)
+
+#Phase de messages
+message = "Salut c'est le deuxième client"
+print(f"Client : {message}")
+client_socket.send(message.encode())
+
+reply = client_socket.recv(1024).decode()
+print(f"Server : {reply}")
+
+message = "arret"
+print(f"Client : {message}")
+client_socket.send(message.encode())
+bye()
