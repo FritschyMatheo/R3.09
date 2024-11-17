fba62d96f82d4aec3a995c00f7d90eed73e3e0f4
Matheo-Fixe <matheo.fritschy13@gmail.com>
2024-11-17 19:11:22 +0100

    Debut graphique, il faut mettre python version 3.10 pour que ça lance

--- R3.09/Interface-Graphique/Exercice2.py
+++ R3.09/Interface-Graphique/Exercice2.py
@@ -1,9 +1,11 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QHBoxLayout
 app = QApplication(sys.argv)
 root = QWidget()
 root.resize(250, 250)
-root.setWindowTitle("Une première fenêtre")
+root.setWindowTitle("Conversion de Température")
+#case = QHBoxLayout()
+
 root.show()
 if __name__ == '__main__':
     sys.exit(app.exec())
