534aea5ccee1645532ce4b003b8390e85d8918c1
Mathéo FRITSCHY <matheo.fritschy@altodis.fr>
2024-11-22 15:41:23 +0100

    Exercice 1 graphique ok

--- R3.09/Interface-Graphique/Exercice1.py
+++ R3.09/Interface-Graphique/Exercice1.py
@@ -1,9 +1,43 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
-app = QApplication(sys.argv)
-root = QWidget()
-root.resize(250, 250)
-root.setWindowTitle("Une première fenêtre")
-root.show()
+from PyQt6.QtWidgets import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        widget = QWidget()
+        self.setCentralWidget(widget)
+        grid = QGridLayout()
+        widget.setLayout(grid)
+        self.setWindowTitle("Une première fenêtre")
+
+        lab = QLabel("Saisir votre nom")
+        self.text = QLineEdit("")
+        self.retour = QLabel("")
+        ok = QPushButton("Ok")
+        quit = QPushButton("Quitter")
+
+        # Ajouter les composants au grid ayout
+        grid.addWidget(lab, 0, 0) 
+        grid.addWidget(self.text, 1, 0)
+        grid.addWidget(ok, 2, 0)
+        grid.addWidget(self.retour, 3, 0)
+        grid.addWidget(quit, 4, 0)
+
+        ok.clicked.connect(self.__actionOk)
+        quit.clicked.connect(self.__actionQuitter)
+
+    def __actionOk(self):
+        nom = self.text.text()
+        if nom == "":
+            self.retour.setText("Veuillez entrer un nom :)")
+        else:
+            self.retour.setText(f"Bonjour {nom} !")
+    def __actionQuitter(self):
+        QApplication.exit(0)
+
 if __name__ == '__main__':
-    sys.exit(app.exec())
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.resize(300,150)
+    window.show()
+    app.exec()
